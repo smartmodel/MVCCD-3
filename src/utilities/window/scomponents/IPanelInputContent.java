ba5106eba75108f8c0716f5e5bc7417f50bc82b9
pa-sunier <pa.sunier@smartmodel.ch>
2020-06-12 14:16:06 +0200

    2020.06.12 Correctifs de base (Après changement)

--- src/utilities/window/scomponents/IPanelInputContent.java
+++
@@ -1,9 +0,0 @@
-package utilities.window.scomponents;
-
-import utilities.window.editor.DialogEditor;
-
-public interface IPanelInputContent {
-
-    public boolean isDataInitialized();
-    public DialogEditor getEditor();
-}
