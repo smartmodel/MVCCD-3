61d599925471ee1d5c3963babe794b4e80be00a8
RSTiago <tiago.rodriguessoares@he-arc.ch>
2020-06-12 21:22:23 +0200

    Mise en page de la palette + création des composants graphiques qui n'avait pas encore étét créer.

--- src/diagram/mcd/MCDPalette.java
+++ src/diagram/mcd/MCDPalette.java
@@ -6,6 +6,7 @@ import main.MVCCDWindow;
 import mcd.*;
 import mcd.interfaces.IMCDModel;
 import mcd.services.IMCDModelService;
+import preferences.Preferences;
 import repository.editingTreat.mcd.MCDAssociationEditingTreat;
 import repository.editingTreat.mcd.MCDEntityEditingTreat;
 
@@ -36,35 +37,54 @@ public class MCDPalette {
         //TODO-1 A affiner
         panelPalette.removeAll();
 
-        //la palette doit être un BoxLayout
-        panelPalette.setLayout(new BoxLayout(panelPalette,BoxLayout.Y_AXIS));
+        //Le flowLayout va alligner tous les composants sur l'axe Y
+        panelPalette.setLayout(new FlowLayout());
         //Changement de la couleur de fond
         panelPalette.setBackground(Color.WHITE);
+        panelPalette.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        panelPalette.setPreferredSize((new Dimension(185, Preferences.PREFERENCES_WINDOW_HEIGHT)));
 
-        JLabel labelName = new JLabel("Palette");
+        //Dimension pour la taille des bouttons ainsi ils ont tous la même taille
+        Dimension btnDimension = new Dimension(175,60);
+
+        JLabel labelName = new JLabel("Palette ");
+
+        //Création du bouton Entité et ajout de son listener
         JButton btnEntite = createEntityBtn();
         btnEntite.addActionListener(this::actionPerformedEntity);
 
         JButton btnAssociation = createAssociationBtn();
         btnAssociation.addActionListener(this::actionPerformedAssociation);
 
+        JButton btnEntiteAssociative = createEntiteAssociativeBtn();
+        JButton btnNote = createNoteBtn();
+        JButton btnGenSpe = createGenSpeBtn();
+        JButton btnAncre = createAncreBtn();
+        JButton btnContrainteOCL = createContrainteOCLBtn();
 
-
-
+        //Ajout des boutons dans la palette
         panelPalette.add(labelName);
-        panelPalette.add(btnEntite);
-        panelPalette.add(btnAssociation);
+        panelPalette.add(btnEntite).setPreferredSize(btnDimension);
+        panelPalette.add(btnAssociation).setPreferredSize(btnDimension);
+        panelPalette.add(btnEntiteAssociative).setPreferredSize(btnDimension);
+        panelPalette.add(btnNote).setPreferredSize(btnDimension);
+        panelPalette.add(btnGenSpe).setPreferredSize(btnDimension);
+        panelPalette.add(btnAncre).setPreferredSize(btnDimension);
+        panelPalette.add(btnContrainteOCL).setPreferredSize(btnDimension);
     }
 
     //Création du boutton entité
     public JButton createEntityBtn(){
         JButton btnEntite = new JButton();
-        btnEntite.setBackground(Color.white);
+
         try {
-            Image img = ImageIO.read(getClass().getResource("entite.PNG")).getScaledInstance(75,45,1);
+            btnEntite.setHorizontalAlignment(SwingConstants.LEFT);
+            Image img = ImageIO.read(getClass().getResource("entite2.PNG")).getScaledInstance(60,35,4);
             btnEntite.setIcon(new ImageIcon(img));
             btnEntite.setText("Entité");
-            btnEntite.setHorizontalTextPosition(SwingConstants.EAST);
+            btnEntite.setHorizontalTextPosition(SwingConstants.RIGHT);
+
+
 
         } catch (Exception ex) {
             System.out.println(ex);
@@ -76,17 +96,103 @@ public class MCDPalette {
     //Création du boutton associtaion
     public JButton createAssociationBtn(){
         JButton btnAssociation = new JButton();
-        btnAssociation.setBackground(Color.white);
         try {
-            Image img = ImageIO.read(getClass().getResource("association.PNG")).getScaledInstance(45,35,1);
+            btnAssociation.setHorizontalAlignment(SwingConstants.LEFT);
+            Image img = ImageIO.read(getClass().getResource("association.png")).getScaledInstance(55,35,4);
             btnAssociation.setIcon(new ImageIcon(img));
             btnAssociation.setText("Association");
-            btnAssociation.setHorizontalTextPosition(SwingConstants.EAST);
+            btnAssociation.setHorizontalTextPosition(SwingConstants.RIGHT);
+
 
         } catch (Exception ex) {
             System.out.println(ex);
         }
         return btnAssociation;
+
+    }
+
+    //Création du boutton entité associative
+    public JButton createEntiteAssociativeBtn(){
+        JButton btnEntiteAssociative = new JButton("<html>Entité<br/>Associative</html>");
+        try {
+            btnEntiteAssociative.setHorizontalAlignment(SwingConstants.LEFT);
+            Image img = ImageIO.read(getClass().getResource("entiteAssociative.PNG")).getScaledInstance(60,35,4);
+            btnEntiteAssociative.setIcon(new ImageIcon(img));
+            btnEntiteAssociative.setHorizontalTextPosition(SwingConstants.RIGHT);
+
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        return btnEntiteAssociative;
+
+    }
+
+    //Création du boutton note
+    public JButton createNoteBtn(){
+        JButton btnNote = new JButton("Note");
+        try {
+            btnNote.setHorizontalAlignment(SwingConstants.LEFT);
+            Image img = ImageIO.read(getClass().getResource("notes.PNG")).getScaledInstance(60,35,4);
+            btnNote.setIcon(new ImageIcon(img));
+            btnNote.setHorizontalTextPosition(SwingConstants.RIGHT);
+
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        return btnNote;
+
+    }
+
+    //Création du boutton de généralisation et spécialisation
+    public JButton createGenSpeBtn(){
+        JButton btnGenSpe = new JButton("<html>Généralisation /<br/>Spécialisation</html>");
+        try {
+            btnGenSpe.setHorizontalAlignment(SwingConstants.LEFT);
+            Image img = ImageIO.read(getClass().getResource("gen_spe.PNG")).getScaledInstance(60,35,4);
+            btnGenSpe.setIcon(new ImageIcon(img));
+            btnGenSpe.setHorizontalTextPosition(SwingConstants.RIGHT);
+
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        return btnGenSpe;
+
+    }
+
+    //Création du boutton ancre
+    public JButton createAncreBtn(){
+        JButton btnAncre = new JButton("Ancre");
+        try {
+            btnAncre.setHorizontalAlignment(SwingConstants.LEFT);
+            Image img = ImageIO.read(getClass().getResource("ancre.PNG")).getScaledInstance(60,35,4);
+            btnAncre.setIcon(new ImageIcon(img));
+            btnAncre.setHorizontalTextPosition(SwingConstants.RIGHT);
+
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        return btnAncre;
+
+    }
+
+    //Création du boutton de contrainte OCL
+    public JButton createContrainteOCLBtn(){
+        JButton btnContrainteOCL = new JButton("<html>Contrainte<br/> OCL</html>");
+        try {
+            btnContrainteOCL.setHorizontalAlignment(SwingConstants.LEFT);
+            Image img = ImageIO.read(getClass().getResource("ocl.PNG")).getScaledInstance(60,35,4);
+            btnContrainteOCL.setIcon(new ImageIcon(img));
+            btnContrainteOCL.setHorizontalTextPosition(SwingConstants.RIGHT);
+
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        return btnContrainteOCL;
 
     }
 
