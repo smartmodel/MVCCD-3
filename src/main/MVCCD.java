5bd70c5da195fd22c163f9076d3f732b5f435727
ANTOINE FREY <antoine.frey@he-arc.ch>
2021-06-07 11:00:41 +0200

    Ajout de la librairie graphique flatlaf

--- src/main/MVCCD.java
+++ src/main/MVCCD.java
@@ -1,11 +1,13 @@
 package main;
 
+import com.formdev.flatlaf.FlatLightLaf;
 import console.ViewLogsManager;
 import messages.MessagesBuilder;
 import resultat.Resultat;
 import resultat.ResultatElement;
 import resultat.ResultatLevel;
 
+import javax.swing.*;
 import java.io.Console;
 
 public class MVCCD  {
@@ -14,8 +16,8 @@ public class MVCCD  {
      * Démarre MVC-CD-3
      */
     public static void main(String[] args) {
-        Console console;
         try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
             MVCCDManager.instance().start();
         } catch(Exception e){
             if (MVCCDManager.instance().getConsoleManager() != null) {
