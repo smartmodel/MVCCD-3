532256c72a9936b33163d3de4115bcd912903e46
ANTOINE FREY <antoine.frey@he-arc.ch>
2021-06-18 14:04:38 +0200

    Ajout des autres icônes dans le référentiel

--- src/main/window/repository/WinRepository.java
+++ src/main/window/repository/WinRepository.java
@@ -3,13 +3,12 @@ package main.window.repository;
 import jiconfont.icons.font_awesome.FontAwesome;
 import jiconfont.swing.IconFontSwing;
 import main.MVCCDElementApplicationPreferences;
-import mcd.MCDAttribute;
-import mcd.MCDContDiagrams;
-import mcd.MCDEntity;
-import mldr.MLDRModel;
+import mcd.*;
+import mldr.*;
+import mpdr.MPDRColumn;
 import mpdr.MPDRModel;
-import utilities.window.PanelBorderLayout;
-import utilities.window.PanelBorderLayoutResizer;
+import mpdr.MPDRTable;
+import project.Project;
 
 import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
@@ -53,24 +52,50 @@ public class WinRepository extends JPanel {
                 Component c = super.getTreeCellRendererComponent(tree, value, selected, expanded, isLeaf, row, focused);
                 DefaultMutableTreeNode node = (DefaultMutableTreeNode) value;
 
+                // Icone de projet
+                if(node.getUserObject() instanceof Project){
+                    Icon imageIcon = new ImageIcon(new ImageIcon(uriImage + "project.png").getImage().getScaledInstance(18, 15, Image.SCALE_DEFAULT));
+                    setIcon(imageIcon);
+                }
+                // Icone préférences
                 if(node.getUserObject() instanceof MVCCDElementApplicationPreferences){
                     Icon icon = IconFontSwing.buildIcon(FontAwesome.COG, 15);
                     setIcon(icon);
                 }
-                if(node.getUserObject() instanceof MCDAttribute){
+                // Icone entité
+                if(node.getUserObject() instanceof MCDEntity || node.getUserObject() instanceof MLDRTable){
                     Icon imageIcon = new ImageIcon(new ImageIcon(uriImage + "entity.png").getImage().getScaledInstance(12, 15, Image.SCALE_DEFAULT));
                     setIcon(imageIcon);
                 }
-                if(node.getUserObject() instanceof MCDEntity){
-                    Icon imageIcon = new ImageIcon(new ImageIcon(uriImage + "entity.png").getImage().getScaledInstance(12, 15, Image.SCALE_DEFAULT));
+                // Icone attribut
+                if(node.getUserObject() instanceof MCDAttribute || node.getUserObject() instanceof MLDRColumn){
+                    Icon imageIcon = new ImageIcon(new ImageIcon(uriImage + "mldr-entity-element.png").getImage().getScaledInstance(15, 15, Image.SCALE_DEFAULT));
                     setIcon(imageIcon);
                 }
+                // Icone contrainte d'association
+                if(node.getUserObject() instanceof MCDAssociation || node.getUserObject() instanceof MCDAssEnd || node.getUserObject() instanceof MLDRPK
+                        || node.getUserObject() instanceof MLDRFK){
+                    Icon imageIcon = new ImageIcon(new ImageIcon(uriImage + "mldr-entity-constraint.png").getImage().getScaledInstance(15, 15, Image.SCALE_DEFAULT));
+                    setIcon(imageIcon);
+                }
+                // Icone MLDR-R
                 if(node.getUserObject() instanceof MCDContDiagrams || node.getUserObject() instanceof MLDRModel){
                     Icon imageIcon = new ImageIcon(new ImageIcon(uriImage + "MLD-R.png").getImage().getScaledInstance(15,15, Image.SCALE_DEFAULT));
                     setIcon(imageIcon);
                 }
+                // Icone MPD-R
                 if(node.getUserObject() instanceof MPDRModel){
                     Icon imageIcon = new ImageIcon(new ImageIcon(uriImage + "MPD-R.png").getImage().getScaledInstance(15,15, Image.SCALE_DEFAULT));
+                    setIcon(imageIcon);
+                }
+                // Icone entité MPD-R
+                if(node.getUserObject() instanceof MPDRTable){
+                    Icon imageIcon = new ImageIcon(new ImageIcon(uriImage + "entity-mpdr.png").getImage().getScaledInstance(18,15, Image.SCALE_DEFAULT));
+                    setIcon(imageIcon);
+                }
+                // Icone element MPD-R
+                if(node.getUserObject() instanceof MPDRColumn){
+                    Icon imageIcon = new ImageIcon(new ImageIcon(uriImage + "mpdr-entity-element.png").getImage().getScaledInstance(12,15, Image.SCALE_DEFAULT));
                     setIcon(imageIcon);
                 }
 
