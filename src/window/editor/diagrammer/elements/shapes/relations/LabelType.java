91e7519e6ab4a15a49ad462b87caa13ff860c51a
Melvyn Vogelsang <melvyn.vogelsang@gmail.com>
2022-01-11 17:33:41 +0100

    Ajout des types de labels pour le diagrammeur

---
+++ src/window/editor/diagrammer/elements/shapes/relations/LabelType.java
@@ -0,0 +1,9 @@
+package window.editor.diagrammer.elements.shapes.relations;
+
+public enum LabelType {
+    SOURCE_ROLE,
+    DESTINATION_ROLE,
+    SOURCE_CARDINALITY,
+    DESTINATION_CARDINALITY,
+    ASSOCIATION_NAME
+}
